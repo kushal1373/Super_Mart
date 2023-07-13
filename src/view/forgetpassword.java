511ab5436e4d8c751fe85c281e18c2323b348def
Suyogya17 <suyogyashrestha17@gmail.com>
2023-07-13 18:29:28 +0545

    Error

--- src/view/forgetpassword.java
+++ src/view/forgetpassword.java
@@ -167,7 +167,7 @@ forgetmodel model;
     private void bttnbackActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_bttnbackActionPerformed
         // TODO add your handling code here:
         setVisible(false);
-        ManagerView r = new ManagerView();
+        LoginView r = new LoginView();
         r.setVisible(true);
     }//GEN-LAST:event_bttnbackActionPerformed
 public forgetmodel getUser()
